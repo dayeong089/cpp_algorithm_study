0b8bc614d3e21ae1310a4736f9a08ac605a62a84
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2024-01-31 18:52:28 +0900

    [Gold V] Title: 괄호의 값, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.cc
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string str;
+    cin >> str;
+    int result = 0;
+    stack<int> s;
+
+    for(auto c : str)
+    {
+        if(c == '(')
+        {
+            s.push(-1);
+        }
+        else if(c == '[')
+        {
+            s.push(-2);
+        }
+        else if(c == ')')
+        {
+            int val = 0;
+            while(true)
+            {
+                if(s.empty() || s.top()==-2)
+                {
+                    cout << '0' << '\n';
+                    return 0;
+                }
+                else
+                {
+                    if(s.top() == -1)
+                    {
+                        s.pop();
+                        if(val == 0) s.push(2);
+                        else s.push(val*2);
+                        break;
+                    }
+                    else
+                    {
+                        val += (s.top());
+                        s.pop();
+                    }
+                }
+            }
+        }
+        else if(c == ']')
+        {
+            int val = 0;
+            while(true)
+            {
+                if(s.empty() || s.top()==-1)
+                {
+                    cout << '0' << '\n';
+                    return 0;
+                }
+                else
+                {
+                    if(s.top() == -2)
+                    {
+                        s.pop();
+                        if(val == 0) s.push(3);
+                        else {
+                            s.push(val*3);
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        val += (s.top());
+                        s.pop();
+                    }
+                }
+            }
+        }
+    }
+
+
+    while(!s.empty())
+    {
+        if(s.top()<0)
+        {
+            cout << '0' << '\n';
+            return 0;
+        }
+        result += s.top();
+        s.pop();
+    }
+
+    cout << result << '\n';
+
+    return 0;
+}
