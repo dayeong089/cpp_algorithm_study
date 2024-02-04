a2c9d4ccef82f0499ec60d318139eab6cad809f1
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2024-02-04 21:00:01 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 44 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[100005];
+int dp[100005];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+
+    for(int i=1; i<=n; i++)
+    {
+        cin >> arr[i];
+        dp[i] = dp[i-1] + arr[i];
+    }
+    
+    while(m--)
+    {
+        int i, j;
+        cin >> i >> j;
+        cout << dp[j] - dp[i-1] << '\n';
+    }
+
+    return 0;
+}
