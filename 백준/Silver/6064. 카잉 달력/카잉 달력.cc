393c5d2a1fc5cdc5520796eaaf1893a889aa4d80
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2024-02-05 18:56:42 +0900

    [Silver I] Title: 카잉 달력, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.cc
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -1,17 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int gcd(int a, int b)
-{
-    if(b == 0) return a;
-    return gcd(b, a%b);
-}
-
-int lcm(int a, int b)
-{
-    return (a / gcd(a, b)) * b;
-}
-
 int main()
 {
     ios::sync_with_stdio(0);
@@ -22,23 +11,21 @@ int main()
 
     while(t--)
     {
+        bool check = false;
         cin >> m >> n >> x >> y;
         if(x == m) x = 0;
         if(y == n) y = 0;
-        int l = lcm(m, n);
-        bool exist = false;
-
-        for(int i=x; i<=l; i+=m)
+        for(int i=x; i<=n*m; i+=m)
         {
             if(i==0) continue;
             if(i%n == y)
             {
-                exist = true;
                 cout << i << '\n';
+                check = true;
+                break;
             }
         }
-
-        if(!exist) cout << "-1" << '\n';
+        if(!check) cout << "-1" << '\n';
     }
 
     return 0;
