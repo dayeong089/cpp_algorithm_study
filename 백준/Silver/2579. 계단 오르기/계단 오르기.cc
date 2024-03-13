1ac4a523542a5f1537d59d824f0faf4b283d17f4
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2024-03-13 11:29:48 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,4 +1,7 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+
 using namespace std;
 
 int n;
@@ -7,23 +10,25 @@ int dp[305];
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    cin >> n;
-    for(int i=1; i<=n; i++) cin >> arr[i];
-
-    dp[1] = arr[1];
-    if(n>1) dp[2] = arr[1] + arr[2];
-    if(n>2) dp[3] = max({arr[1] + arr[3], arr[2] + arr[3]});
-    if(n>3)
-    {
-        for(int i=4; i<=n; i++)
-        {
-            dp[i] = max({dp[i-3] + arr[i-1], dp[i-2]}) + arr[i];
-        }
-    }
-
-    cout << dp[n];
-    return 0;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n;
+	for(int i=1; i<=n; i++)
+	{
+		cin >> arr[i];
+	}
+	
+	dp[1] = arr[1];
+	dp[2] = arr[1] + arr[2];
+	dp[3] = max(arr[1], arr[2]) + arr[3];
+	
+	for(int i=4; i<=n; i++)
+	{
+		dp[i] = max(dp[i-2], dp[i-3] + arr[i-1]) + arr[i];
+	}
+	
+	cout << dp[n];
+	
+	return 0;
 }
