5ca94a8f075de260f0569d224225724d6f103946
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2024-04-24 22:37:29 +0900

    [Silver IV] Title: 로프, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.cc
+++ 백준/Silver/2217. 로프/로프.cc
@@ -1,8 +1,10 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+
 using namespace std;
 
 int n;
-int arr[100005];
+int arr[100001];
 
 int main()
 {
@@ -14,16 +16,13 @@ int main()
 
     sort(arr, arr+n);
 
-    int max_weight = 0;
-    int now_num = n;
-
+    long long max_weight = 0; 
     for(int i=0; i<n; i++)
     {
-        int now_weight = arr[i] * (n-i);
-        if(now_weight > max_weight) max_weight = now_weight;
+        int now = arr[i] * (n-i);
+        if(now > max_weight) max_weight = now;
     }
 
-    cout << max_weight;
-
+    cout << max_weight << '\n';
     return 0;
 }
