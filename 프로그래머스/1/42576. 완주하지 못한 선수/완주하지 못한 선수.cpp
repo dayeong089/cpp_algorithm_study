ac56cb120d8e710e386655b8cc7126616a20f8b4
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2024-04-28 21:35:05 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 46.52 ms, Memory: 32.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -1,18 +1,45 @@
+// #include <string>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+// string solution(vector<string> participant, vector<string> completion) {
+//     sort(participant.begin(), participant.end());
+//     sort(completion.begin(), completion.end());
+    
+//     int i = 0;
+//     for(; i<completion.size(); i++)
+//     {
+//         if(participant[i] != completion[i]) return participant[i];
+//     }
+    
+//     return participant[i];
+// }
+
+
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
-    sort(participant.begin(), participant.end());
-    sort(completion.begin(), completion.end());
+    unordered_map<string, int> hmap;
     
-    int idx = 0;
-    while(true)
+    for(auto ele : completion)
     {
-        if(idx == completion.size()) return participant[idx];
-        if(participant[idx] == completion[idx]) idx++;
-        else return participant[idx];
+        if(hmap.find(ele) == hmap.end()) hmap.insert({ele, 1});
+        else hmap[ele]++;
+    }
+    
+    for(auto ele : participant)
+    {
+        if(hmap.find(ele) == hmap.end()) return ele;
+        else 
+        {
+            hmap[ele]--;
+            if(hmap[ele] < 0) return ele;
+        }
     }
 }
