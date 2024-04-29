a70e32c43e108e0aca81e77fe7151477a86a9ce5
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2024-04-29 16:54:51 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string new_id) {
+    string answer = "";
+    
+    for(int i=0; i<new_id.size(); i++)
+    {
+        if(new_id[i] >= 'A' && new_id[i] <= 'Z') answer += (new_id[i] - 'A' + 'a');
+        else if(new_id[i] >= 'a' && new_id[i] <= 'z') answer += new_id[i];
+        else if(new_id[i] >= '0' && new_id[i] <= '9') answer += new_id[i];
+        else if(new_id[i] == '-' || new_id[i] == '_') answer += new_id[i];
+        else if(new_id[i] == '.' && answer == "") continue;
+        else if(new_id[i] == '.' && answer[answer.size()-1] == '.') continue;
+        else if(new_id[i] == '.') answer += new_id[i];
+    }
+    
+    if(answer == "") answer = "a";
+    if(answer.size() >= 16) answer = answer.substr(0, 15);
+    if(answer[answer.size()-1] == '.') answer = answer.substr(0, answer.size()-1);
+    if(answer.size() == 1) answer += (answer + answer);
+    if(answer.size() == 2) answer += answer[answer.size()-1];
+    
+    return answer;
+}
