99bb5c4ffbbf14824e171afeea6e8ac86a068d3b
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2024-05-01 15:10:33 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    unordered_map<string, int> closet;
+    
+    for(int i=0; i<clothes.size(); i++)
+    {
+        closet[clothes[i][1]]++;
+    }
+    
+    for(auto it=closet.begin(); it!=closet.end(); it++)
+    {
+        answer *= (it->second + 1);
+    }
+    
+    return answer-1;
+}
