4764f5dfac0b6b8dab42fe295767446d1bb109d6
dayeong089 <64959224+dayeong089@users.noreply.github.com>
2024-05-13 21:59:18 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+int n, m;
+int arr[10];
+bool isvisit[10];
+
+void func(int x)
+{
+    if(x == m)
+    {
+        for(int i=0; i<m; i++) cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for(int i=1; i<=n; i++)
+    {
+        if(isvisit[i]) continue;
+        if(x!=0 && arr[x-1] > i) continue;
+        arr[x] = i;
+        isvisit[i] = true;
+        func(x+1);
+        isvisit[i] = false;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    func(0);
+    return 0;
+}
